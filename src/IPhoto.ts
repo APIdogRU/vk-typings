20617c492bae13ec10a201aaa17c18a63c304633
Vladislav Veluga <vladislav805@yandex.com>
2023-09-10 23:18:28 +0300

    1.0.15: небольшие уточнения IPhoto

--- src/IPhoto.ts
+++ src/IPhoto.ts
@@ -7,20 +7,27 @@ export interface IPhoto {
     id: number;
     access_key?: string;
     album_id: number;
-    user_id: number;
+    /** Только при owner_id < 0 */
+    user_id?: number;
     text: string;
     date: number;
     sizes: Array<IPhotoSizeRequireType<IPhotoSizeRequireSource<IPhotoSize, 'url'>>>;
     width?: number;
     height?: number;
     has_tags?: boolean;
-    likes: ILikesInfo;
-    reposts: IRepostsInfo;
+    /** Только при extended=1 */
+    can_comment?: boolean;
+    can_repost?: boolean;
+    can_be_owner_photo?: boolean; // 01
+    lat?: number;
+    long?: number;
+    orig_photo?: Omit<IPhotoSizeRequireSource<IPhotoSize, 'url'>, 'type'>;
     comments?: {
         count: number;
     };
-    can_comment?: boolean;
-    can_repost?: boolean;
-    lat?: number;
-    long?: number;
+    tags?: {
+        count: number;
+    };
+    likes: ILikesInfo;
+    reposts: IRepostsInfo;
 }
