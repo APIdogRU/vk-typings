463ca50ce8a15366f266171f57d6ba6067b09bc4
Vladislav Veluga <vladislav805@yandex.com>
2023-09-14 22:17:04 +0300

    1.0.16: IGiftItemMarket update

--- src/IGiftItemMarket.ts
+++ src/IGiftItemMarket.ts
@@ -1,13 +1,24 @@
 import type { IGift } from './IGift';
 
 export interface IGiftItemMarket {
-    gift: IGift;
-    payment_type: 'balance';
-    price: number;
-    price_str: string;
+    gift: IGift & {
+        stickers_product_id?: number;
+        keywords?: string;
+    };
+    /** Отсутствует, если disabled=1 */
+    payment_type?: 'balance';
+    /** Отсутствует, если disabled=1 */
+    price?: number;
+    /** Отсутствует, если disabled=1 */
+    price_str?: string;
     gifts_left?: number;
     real_price?: number;
     real_price_str?: string;
     description?: string;
     disabled?: boolean;
+    sticker_pack?: {
+        title: string;
+        description: string;
+        author: string;
+    };
 }
