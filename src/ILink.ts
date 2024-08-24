b5fe81ddee163aa220e194c7e496f62151d890d5
Vladislav Veluga <vladislav805@yandex.com>
2024-08-25 00:34:25 +0300

    v1.0.25

--- src/ILink.ts
+++ src/ILink.ts
@@ -1,16 +1,22 @@
+import type { IApplication } from './IApplication';
 import type { ILinkButton } from './ILinkButton';
 import type { IPhoto } from './IPhoto';
 
 export interface ILink {
-    url: string;
+    /** Отсутствует при наличии app */
+    url?: string;
     title: string;
     caption?: string;
     description: string;
     photo?: IPhoto;
     product?: unknown; // TODO
     button?: ILinkButton;
+    button_text?: string;
     preview_page?: string;
     preview_url?: string;
     target?: 'internal'; // external?
     is_favorite?: boolean;
+    app?: IApplication;
+    is_default_description?: boolean;
+    is_default_icon?: boolean;
 }
